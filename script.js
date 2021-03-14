09bbe50eb9da9ce551628af80637c4e0b9a50b67
MBSinterns000 <56572066+MBSinterns000@users.noreply.github.com>
2021-03-13 23:59:13 -0500

    Fixed Lint errors

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var firstInput = document.getElementById('fname')
-var secondInput = document.getElementById('lname')
-var discribeInput = document.getElementById('discription')
-var emailInput = document.getElementById('email')
-var phoneInput = document.getElementById('phone')
-var madlibParagraph1 = document.getElementById('madlib1')
-var madlibParagraph2 = document.getElementById('madlib2')
+const firstInput = document.getElementById('fname')
+const secondInput = document.getElementById('lname')
+const describeInput = document.getElementById('description')
+const emailInput = document.getElementById('email')
+const phoneInput = document.getElementById('phone')
+const madlibParagraph1 = document.getElementById('madlib1')
+const madlibParagraph2 = document.getElementById('madlib2')
 
-var updateMadlib = function () {
-  var fname = firstInput.value
-  var lname = secondInput.value
-  var discription = discribeInput.value
-  var email = emailInput.value
-  var phone = phoneInput.value
-  var string = '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' + ' ' + '<p>' + discription + '</p>' + ' <p>If you are interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank ">' +
+const updateMadlib = function () {
+  const fname = firstInput.value
+  const lname = secondInput.value
+  const description = describeInput.value
+  const email = emailInput.value
+  const phone = phoneInput.value
+  const string = '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' + ' ' + '<p>' + description + '</p>' + ' <p>If you are interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank ">' +
     email + '</a>' + ' ' + 'or give me a call at <a href="tel:' + phone + ' " target = "_blank" > ' + phone + ' </a>' + '</p > '
 
   madlibParagraph1.innerHTML = string
@@ -21,6 +21,6 @@ var updateMadlib = function () {
 
 firstInput.addEventListener('input', updateMadlib)
 secondInput.addEventListener('input', updateMadlib)
-discribeInput.addEventListener('input', updateMadlib)
+describeInput.addEventListener('input', updateMadlib)
 emailInput.addEventListener('input', updateMadlib)
 phoneInput.addEventListener('input', updateMadlib)
