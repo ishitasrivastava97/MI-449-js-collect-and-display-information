559685a8a13c800a45bbfe908ba21f05050c68b5
MBSinterns000 <56572066+MBSinterns000@users.noreply.github.com>
2021-03-13 23:37:47 -0500

    Uploading dating website

---
+++ script.js
@@ -0,0 +1,26 @@
+var firstInput = document.getElementById('fname')
+var secondInput = document.getElementById('lname')
+var discribeInput = document.getElementById('discription')
+var emailInput = document.getElementById('email')
+var phoneInput = document.getElementById('phone')
+var madlibParagraph1 = document.getElementById('madlib1')
+var madlibParagraph2 = document.getElementById('madlib2')
+
+var updateMadlib = function () {
+  var fname = firstInput.value
+  var lname = secondInput.value
+  var discription = discribeInput.value
+  var email = emailInput.value
+  var phone = phoneInput.value
+  var string = '<h1>Hi, my name is ' + fname + ' ' + lname + '</h1>' + ' ' + '<p>' + discription + '</p>' + ' <p>If you are interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank ">' +
+    email + '</a>' + ' ' + 'or give me a call at <a href="tel:' + phone + ' " target = "_blank" > ' + phone + ' </a>' + '</p > '
+
+  madlibParagraph1.innerHTML = string
+  madlibParagraph2.textContent = string
+}
+
+firstInput.addEventListener('input', updateMadlib)
+secondInput.addEventListener('input', updateMadlib)
+discribeInput.addEventListener('input', updateMadlib)
+emailInput.addEventListener('input', updateMadlib)
+phoneInput.addEventListener('input', updateMadlib)
